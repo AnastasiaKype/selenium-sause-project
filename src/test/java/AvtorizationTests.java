905fa2b3b39d78ed02acc6caba98d8883c3b8923
AnastasiaKype <black_cat123@mail.ru>
2022-05-18 19:08:56 +0300

    initial version

---
+++ src/test/java/AvtorizationTests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+
+public class AvtorizationTests {
+
+static WebDriver driver;
+@BeforeAll
+    static void beforeAll {
+
+    }
+}
